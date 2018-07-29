c37f5dc0b270769399a6de7affcf2c2b2d4063f8
Simranjeet Dua <simranjeet.dua.12@gmail.com>
2018-07-29 11:57:53 +0530

    tkinter practice
    getting hands dirty on tkinter so as to use it properly in the database application

---
+++ KtinterPractice/script1.py
@@ -0,0 +1,18 @@
+#practice script to get familiar with tkinter library to create GUI
+
+from tkinter import *
+
+#it has two parts one being the window and the other being the the widgets
+
+window=Tk() #window part
+
+b1=Button(window,text="Execute")
+b1.grid(row=0,column=0)  #simple button object
+
+e1=Entry(window)
+e1.grid(row=0,column=1) #simple entry object
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=0,column=2) #simple text output object with edited height and width
+
+window.mainloop()
